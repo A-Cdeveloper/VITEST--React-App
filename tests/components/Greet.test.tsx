703969d909679f568f5fc4f311c25c1e232f59be
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-04 01:57:46 +0200

    Greet Component Tests

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,20 @@
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import Greet from "../../src/components/Greet";
+
+describe("Greet", () => {
+  it("should render Hello with name if name is provided", () => {
+    render(<Greet name="Aleksandar" />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/aleksandar/i);
+  });
+
+  it("should render login button if name is not provided", () => {
+    render(<Greet />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
