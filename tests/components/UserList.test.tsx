6728f12f8007caf240876844837ccdd19c20b7ed
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-05 16:01:43 +0200

    small changes in UserList test

--- tests/components/UserList.test.tsx
+++ tests/components/UserList.test.tsx
@@ -3,25 +3,25 @@ import UserList from "../../src/components/UserList";
 import { User } from "../../src/entities";
 
 describe("UserList", () => {
+  const users: User[] = [
+    {
+      id: 1,
+      name: "Aleksandar",
+    },
+    {
+      id: 2,
+      name: "Biljana",
+    },
+  ];
+
   it('should render "No users available" if users list is empty', () => {
     render(<UserList users={[]} />);
     expect(screen.getByText(/no users./i)).toBeInTheDocument();
   });
 
   it("should render users list if users list is not empty", () => {
-    const users: User[] = [
-      {
-        id: 1,
-        name: "Aleksandar",
-      },
-      {
-        id: 2,
-        name: "Biljana",
-      },
-    ];
     render(<UserList users={users} />);
-    // const userList = screen.getAllByRole("listitem");
-    // expect(userList).toHaveLength(2);
+
     users.forEach((user) => {
       const link = screen.getByRole("link", { name: user.name });
       expect(link).toBeInTheDocument();
