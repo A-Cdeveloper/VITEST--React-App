1a2b4cba0abdecb0629d49fff753c0f15f6095cb
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-05 16:37:03 +0200

    test for onchange function call - mock function

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,18 +1,13 @@
 // import Onboarding from "../components/Onboarding";
 
-import ExpandableText from "../components/ExpandableText";
+import SearchBox from "../components/SearchBox";
 
 const PlaygroundPage = () => {
   return (
-    <ExpandableText
-      text="Frontend: Next.js - TypeScript, ShadCN UI - for sleek, modern, and user-friendly interfaces.
-Form Handling & Validation: Formik & Yup - ensuring robust and error-free form validation.
-Token Storage: Cookies - providing secure and efficient authentication mechanisms.
-Responsiveness: Fully responsive design, optimized for all devices - be it mobile, tablet, or desktop.
-🌐 Deployment: The application is deployed on Vercel, ensuring fast and reliable hosting with a smooth user experience.
-
-This project allowed me to dive deep into full-stack development, combining my knowledge of frontend and backend technologies to build a seamless user experience. I'm proud of what I've accomplished and eager to apply these skills to future projects.
-"
+    <SearchBox
+      onChange={(text: string) => {
+        console.log(text);
+      }}
     />
   );
 };
---
+++ tests/components/SearchBox.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import SearchBox from "../../src/components/SearchBox";
+import userEvent from "@testing-library/user-event";
+import { input } from "@testing-library/user-event/dist/cjs/event/input.js";
+
+describe("SearchBox", () => {
+  const renderComponent = () => {
+    const onChange = vi.fn(); // mock function
+    const user = userEvent.setup();
+
+    render(<SearchBox onChange={onChange} />);
+    return {
+      input: screen.getByPlaceholderText(/search/i),
+      onChange,
+      user,
+    };
+  };
+
+  it("should render input filed with placeholder for seraching", () => {
+    const { input } = renderComponent();
+    expect(input).toBeInTheDocument();
+  });
+
+  it("should call onChange when Enter is pressed", async () => {
+    const { onChange, input, user } = renderComponent();
+    const searchWord = "Term";
+
+    await user.type(input, searchWord + "{enter}");
+    expect(onChange).toHaveBeenLastCalledWith(searchWord);
+  });
+
+  it("not call onChange when search word is empty and Enter is pressed", async () => {
+    const { onChange, input, user } = renderComponent();
+
+    await user.type(input, "{enter}");
+    expect(onChange).not.toHaveBeenCalled();
+  });
+});
