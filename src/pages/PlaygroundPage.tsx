cadd29ffbf4b0b89417b728fbcbea387dd311631
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-05 15:47:27 +0200

    text exapand/collapse fn

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,20 @@
 // import Onboarding from "../components/Onboarding";
 
-import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return (
+    <ExpandableText
+      text="Frontend: Next.js - TypeScript, ShadCN UI - for sleek, modern, and user-friendly interfaces.
+Form Handling & Validation: Formik & Yup - ensuring robust and error-free form validation.
+Token Storage: Cookies - providing secure and efficient authentication mechanisms.
+Responsiveness: Fully responsive design, optimized for all devices - be it mobile, tablet, or desktop.
+🌐 Deployment: The application is deployed on Vercel, ensuring fast and reliable hosting with a smooth user experience.
+
+This project allowed me to dive deep into full-stack development, combining my knowledge of frontend and backend technologies to build a seamless user experience. I'm proud of what I've accomplished and eager to apply these skills to future projects.
+"
+    />
+  );
 };
 
 export default PlaygroundPage;
---
+++ tests/components/ExpandableText.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from "@testing-library/react";
+import ExpandableText from "../../src/components/ExpandableText";
+import userEvent from "@testing-library/user-event";
+import exp from "constants";
+
+describe("ExpandableText", () => {
+  const limit = 255;
+  const shortText = "A".repeat(limit - 1);
+  const longText = "A".repeat(limit + 1);
+  const truncatedText = "A".repeat(limit) + "...";
+
+  it("should render text if it is less than 255 characters", () => {
+    render(<ExpandableText text={shortText} />);
+    const article = screen.getByText(shortText);
+
+    expect(article).toBeInTheDocument();
+    expect(article).toHaveTextContent(shortText);
+  });
+
+  it("should truncate text if it is longer than 255 characters", () => {
+    render(<ExpandableText text={longText} />);
+    const article = screen.getByText(truncatedText);
+    const button = screen.getByRole("button");
+
+    expect(article).toBeInTheDocument();
+    expect(article).toHaveTextContent(truncatedText);
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/more/i);
+  });
+
+  it("should show more text if button is clicked", async () => {
+    const user = userEvent.setup();
+
+    render(<ExpandableText text={longText} />);
+    const button = screen.getByRole("button");
+    await user.click(button);
+
+    expect(screen.queryByText(longText)).toBeInTheDocument();
+    expect(button).toHaveTextContent(/less/i);
+  });
+
+  it("should show less text if button is clicked", async () => {
+    const user = userEvent.setup();
+
+    render(<ExpandableText text={longText} />);
+    const showMoreButton = screen.getByRole("button", { name: /more/i });
+    await user.click(showMoreButton);
+
+    const showLessButton = screen.getByRole("button", { name: /less/i });
+    await user.click(showLessButton);
+
+    expect(screen.queryByText(truncatedText)).toBeInTheDocument();
+    expect(showMoreButton).toHaveTextContent(/more/i);
+  });
+});
