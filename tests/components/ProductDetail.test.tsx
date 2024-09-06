9553c309386b3bfa8c0db6d9a9ebcddb540ea363
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-06 15:34:49 +0200

    test product details component

---
+++ tests/components/ProductDetail.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+
+import ProductDetail from "../../src/components/ProductDetail";
+import { products } from "../mocks/handlers";
+import { server } from "../mocks/server";
+import { HttpResponse, http } from "msw";
+
+describe("ProductDetail", () => {
+  it("should render product details if id is correct", async () => {
+    render(<ProductDetail productId={1} />);
+
+    const productName = await screen.findByText(new RegExp(products[0].name));
+    expect(productName).toBeInTheDocument();
+    const price = await screen.findByText(
+      new RegExp(products[0].price.toString())
+    );
+    expect(price).toBeInTheDocument();
+  });
+
+  it('should render "The given product was not found" if id is incorrect', async () => {
+    server.use(
+      http.get("/products/1", async () => {
+        return HttpResponse.json(null, { status: 404 });
+      })
+    );
+    render(<ProductDetail productId={1} />);
+    const message = await screen.findByText(/not found/i);
+    expect(message).toBeInTheDocument();
+  });
+
+  it('should render "The given product was not found" if id is 0', async () => {
+    render(<ProductDetail productId={0} />);
+    const message = await screen.findByText(/invalid /i);
+    expect(message).toBeInTheDocument();
+  });
+});
--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,5 +1,12 @@
 import { http, HttpResponse } from "msw";
 
+export const products = [
+  { id: 1, name: "Product 1", price: 10, categoryId: 1 },
+  { id: 2, name: "Product 2", price: 20, categoryId: 1 },
+  { id: 3, name: "Product 3", price: 30, categoryId: 1 },
+  { id: 4, name: "Product 4", price: 40, categoryId: 2 },
+];
+
 export const handlers = [
   http.get("/categories", () => {
     return HttpResponse.json([
@@ -11,11 +18,17 @@ export const handlers = [
     ]);
   }),
   http.get("/products", () => {
-    return HttpResponse.json([
-      { id: 1, name: "Product 1", price: 10, categoryId: 1 },
-      { id: 2, name: "Product 2", price: 20, categoryId: 1 },
-      { id: 3, name: "Product 3", price: 30, categoryId: 1 },
-      { id: 4, name: "Product 4", price: 40, categoryId: 2 },
-    ]);
+    return HttpResponse.json(products);
+  }),
+
+  http.get("/products/:id", ({ params }) => {
+    const { id } = params;
+    const product = products.find((p) => p.id === Number(id));
+
+    if (!product) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    return HttpResponse.json(product);
   }),
 ];
