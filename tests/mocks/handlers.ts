ee3f1aef226d8e6fc6016d4513abfdd0cec71443
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-07 00:59:20 +0200

    create handlers for products from db

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,28 +1,3 @@
-import { http, HttpResponse } from "msw";
-import { products } from "./data";
+import { db } from "./db";
 
-export const handlers = [
-  http.get("/categories", () => {
-    return HttpResponse.json([
-      { id: 1, name: "Electronics" },
-      { id: 2, name: "Appliances" },
-      { id: 3, name: "Accessories" },
-      { id: 4, name: "Books" },
-      { id: 5, name: "Movies" },
-    ]);
-  }),
-  http.get("/products", () => {
-    return HttpResponse.json(products);
-  }),
-
-  http.get("/products/:id", ({ params }) => {
-    const { id } = params;
-    const product = products.find((p) => p.id === Number(id));
-
-    if (!product) {
-      return new HttpResponse(null, { status: 404 });
-    }
-
-    return HttpResponse.json(product);
-  }),
-];
+export const handlers = [...db.product.toHandlers("rest")];
