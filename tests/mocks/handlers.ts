f5c2758dbde4cf2fcd4df282ec7f5f61c7cdd3ca
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-06 11:37:41 +0200

    create mock handler for products

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -10,4 +10,12 @@ export const handlers = [
       { id: 5, name: "Movies" },
     ]);
   }),
+  http.get("/products", () => {
+    return HttpResponse.json([
+      { id: 1, name: "Product 1", price: 10, categoryId: 1 },
+      { id: 2, name: "Product 2", price: 20, categoryId: 1 },
+      { id: 3, name: "Product 3", price: 30, categoryId: 1 },
+      { id: 4, name: "Product 4", price: 40, categoryId: 2 },
+    ]);
+  }),
 ];
