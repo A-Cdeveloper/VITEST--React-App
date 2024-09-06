962f72099067b26d1c67a7608452eea20d277d63
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-07 00:36:46 +0200

    create main test file for testing different things

---
+++ tests/components/main.test.ts
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { faker } from "@faker-js/faker";
+
+describe("main", () => {
+  it("should test", () => {});
+
+  // const product = {
+  //   name: faker.commerce.productName(),
+  //   price: faker.commerce.price(),
+  //   category: faker.commerce.department(),
+  // };
+  // console.log(product);
+});
